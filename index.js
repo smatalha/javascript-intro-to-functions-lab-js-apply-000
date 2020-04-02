6a3cd60c74fb85c7974cfacc53d5f5cdcffa36a0
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-02 02:36:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase){
     return "I can't hear you!"
   } else if (string === string.toUpperCase){
-    return 
+    return "YES INDEED!"
+  }else if (string === "") {
+    
   }
 }
