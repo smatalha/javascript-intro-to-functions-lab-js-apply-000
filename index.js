f818e741ad933afc3641a7a6b183ed9dc4cfb5e4
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-02 02:29:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-console.log(shout)
-console.log(whisper)
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
